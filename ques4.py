8bbf4d63dd6b22d1b985f3b614832614b1403ff4
maneeshpandey <33758090+maneeshpandey@users.noreply.github.com>
2017-11-19 20:41:29 +0530

    Update ques4.py

--- ques4.py
+++ ques4.py
@@ -1,18 +1,18 @@
 import os
 import csv
 for file_ in os.listdir("raw"):
-    with open('raw/'+ file_, 'rb') as csvfile:
+    with open('raw/'+ file_, 'rb') as csvfile:                 #opening file
         
-        spamreader = csv.reader(csvfile, delimiter=',')
+        spamreader = csv.reader(csvfile, delimiter=',')       #reading csv file word by word
         
-        for row in spamreader:
-            if os.path.isfile("processed/" + row[0] +"-processed.csv"):
+        for row in spamreader:                                                  #function for pushing row
+            if os.path.isfile("processed/" + row[0] +"-processed.csv"):         
                 
-                with open("processed/" + row[0] +"-processed.csv", 'a') as csvfile1:
-                    spamwriter = csv.writer(csvfile1, delimiter=',')
-                    spamwriter.writerow(row)
+                with open("processed/" + row[0] +"-processed.csv", 'a') as csvfile1:       
+                    spamwriter = csv.writer(csvfile1, delimiter=',')             #writing to csv file in given format        
+                    spamwriter.writerow(row)                                     #inserting row
             else:
                 
                 with open("processed/" + row[0] +"-processed.csv", 'wb') as csvfile1:
                     spamwriter = csv.writer(csvfile1, delimiter=',')
-                    spamwriter.writerow(row)
+                    spamwriter.writerow(row)                             
