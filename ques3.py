395a42c23e8830d74c2bbd720977d675a408cc47
maneeshpandey <33758090+maneeshpandey@users.noreply.github.com>
2017-11-19 20:36:25 +0530

    Update ques3.py

--- ques3.py
+++ ques3.py
@@ -2,14 +2,14 @@ import time
 import psutil
 
 
-def find_usage_30sec():
+def find_usage_30sec():                                            #for 30 seconds
     threading.Timer(30.0, find_usage_30sec).start()
     print "Usage in last 30 seconds"
-    print psutil.cpu_percent()
-    print psutil.virtual_memory()
-    print psutil.disk_usage('/')
-
-def find_usage():
+    print psutil.cpu_percent()                                      # for cpu usage
+    print psutil.virtual_memory()                                   # for virtual memory 
+    print psutil.disk_usage('/')                                    # for disk usage
+ 
+def find_usage():                                                    #for  5 seconds
     threading.Timer(5.0, find_usage).start()
     print "Current Usage"
     print psutil.cpu_percent()
@@ -17,7 +17,7 @@ def find_usage():
     print psutil.disk_usage('/')
 
 
-def find_usage_5min():
+def find_usage_5min():                                               #for 5 minutes
     threading.Timer(300.0, find_usage_5min).start()
     print "Usage in last 5 minutes"
     print psutil.cpu_percent()
@@ -25,7 +25,7 @@ def find_usage_5min():
     print psutil.disk_usage('/')
     
 def find_usage_1min():
-    threading.Timer(60.0, find_usage_1min).start()
+    threading.Timer(60.0, find_usage_1min).start()                    #for 1 minutes
     print "Usage in last 1 minute"
     print psutil.cpu_percent()
     print psutil.virtual_memory()
